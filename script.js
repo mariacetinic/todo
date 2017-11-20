b6b10e46d42f9ebc3050bf236e3f9c49d09f8bc1
https://github.com/mariacetinic/html5.git <maria.cetinic@gmail.com>
2017-11-20 16:39:54 +0100

    implement jquery

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    sessionStorage.user = "test";  
+    sessionStorage.password = "password";
+
+    /*var numberFromSession1 = Number(sessionStorage.nummer1);
+    var numberFromSession2 = Number(sessionStorage.nummer2);*/
+
+        console.log(sessionStorage.user);
+        console.log(sessionStorage.password);
+});
